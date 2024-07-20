15a6fd46229b45d5b0353d26964ee9df797e5f4f
PHorvath1123 <horvathpeter1996@gmail.com>
2024-07-20 17:26:00 +0200

    Created a Button as ui primitive

---
+++ client/src/components/ui/Button.tsx
@@ -0,0 +1,25 @@
+import Button from '@mui/material/Button';
+
+type ButtonProps = {
+    text: string | number
+};
+
+export default function CustomButton(props: ButtonProps){
+    
+    return(
+        <Button 
+            sx={{
+                backgroundColor: '#EF6950',
+                fontFamily: 'Roboto',
+                height: '4ch',
+                width: '16ch',
+                fontSize: 'clamp(0.6252rem, 0.9172rem + 0.2459vw, 0.8681rem)',
+                ":hover":{
+                    backgroundColor: '#c9371d',
+                },
+            }} 
+            variant='contained'>
+            {props.text}
+        </Button>
+    );
+};
