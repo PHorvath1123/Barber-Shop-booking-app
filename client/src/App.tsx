d3e53b6fa0b1b6db9b6f4e8c5493e4d1e5fb729d
PHorvath1123 <horvathpeter1996@gmail.com>
2024-07-20 12:14:07 +0200

    Header component is renamed.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom";
-import Header from './components/Header'
+import Navbar from './components/ui/Navbar'
 
 export default function App(){
     return(
        <>
-            <Header/>
+            <Navbar/>
             <main className="mt-20">
                 <Outlet></Outlet>
             </main>
