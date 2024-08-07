9aa0ecfad2dbe16bb42aa0abbd61e3ea0eb068ae
PHorvath1123 <horvathpeter1996@gmail.com>
2024-08-07 20:27:11 +0200

    Created a text input ui component

---
+++ client/src/components/ui/TextInput.tsx
@@ -0,0 +1,46 @@
+import TextField from '@mui/material/TextField';
+
+type TextFieldProps  = {
+    label: string
+};
+
+export default function TextInput(props: TextFieldProps){
+
+    return(
+        <TextField
+          id="standard-multiline-flexible"
+          label={props.label}
+          multiline
+          required
+          type='text'
+          maxRows={4}
+          variant="standard"
+          sx={{
+            fontFamily: 'Roboto',
+            '.MuiInput-underline:before': {
+              borderBottomColor: '#D9D9D9', 
+            },
+            '.MuiInput-underline:hover:before': {
+              borderBottomColor: '#D9D9D9 !important', 
+            },
+            '.MuiInput-underline:after': {
+              borderBottomColor: '#EF6950',
+            },
+            '.MuiInputLabel-root': {
+              color: '#D9D9D9',
+               
+            },
+            '.MuiInputLabel-root.Mui-focused': {
+              color: '#D9D9D9', 
+            },
+            '.MuiInputBase-input': {
+              color: '#D9D9D9', 
+              fontSize:'clamp(0.884rem, 1.0736rem + 0.1596vw, 1.0417rem)',
+            },
+            '.MuiInputBase-input:focus': {
+              color: '#D9D9D9', 
+            },
+          }}
+        />
+    );
+}
