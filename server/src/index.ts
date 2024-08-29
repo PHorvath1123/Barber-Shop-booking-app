657b4cdc87372cc2b7f75619fd7520d2ecb82a66
PHorvath1123 <horvathpeter1996@gmail.com>
2024-08-29 21:35:49 +0200

    Add PriceList API route to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Application } from 'express';
 import path from 'path';
 import getBarbersRoute from './routes/barbers.home.route';
+import getPriceListRoute from './routes/priceList.route';
+
 
 const app: Application = express();
 
@@ -9,5 +11,6 @@ const port = 3000;
 app.use(express.json());
 
 app.use('/api/getBarbersToHome', getBarbersRoute);
+app.use('/api/getPriceList', getPriceListRoute);
 
 app.listen(port);
