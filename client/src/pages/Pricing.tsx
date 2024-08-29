e5100a75760c45906c6e2ced709ba57de4d916be
PHorvath1123 <horvathpeter1996@gmail.com>
2024-08-29 21:34:28 +0200

    Wrapped the PriceList component in the QueryClientProvider.

--- client/src/pages/Pricing.tsx
+++ client/src/pages/Pricing.tsx
@@ -1,12 +1,17 @@
 import Navbar from '../components/ui/Navbar';
 import Footer from '../components/Footer';
 import PriceList from '../components/PriceList'
+import { QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+const queryClient = new QueryClient()
 
 export default function Pricing(){
     return(
         <>
             <Navbar></Navbar>
-            <PriceList></PriceList>
+            <QueryClientProvider client={queryClient}>
+                <PriceList></PriceList>
+            </QueryClientProvider>
             <Footer></Footer>
            
         </>
