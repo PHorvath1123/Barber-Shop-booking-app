283f90f5957daca7fd78b06271c5b9fb7ed287b2
PHorvath1123 <horvathpeter1996@gmail.com>
2024-08-29 21:33:05 +0200

    Refactor PriceList component: Added data fetching with React Query and mapped dynamic content

--- client/src/components/PriceList.tsx
+++ client/src/components/PriceList.tsx
@@ -1,34 +1,51 @@
 import PricingStyle from '../styles/pricing/Pricing.module.css';
+import {useQuery} from '@tanstack/react-query';
+
+type PriceType = {
+    title: string;
+    price: number;
+  };
+  
+  type PriceListType = {
+    category: string,
+    services: PriceType[]
+  };
 
 export default function PriceList(){
+
+    const fetchPrice = async (): Promise<PriceListType[]> =>{
+        const response = await fetch('/api/getPriceList');
+        const data: PriceListType[] = await response.json();
+        return data;
+    };
+
+    const {data, isError} = useQuery<PriceListType[]>({ 
+        queryKey: ['priceList'], 
+        queryFn: () => fetchPrice(),
+    });
+
+    {(isError) && <div>An error has occured.</div>}
 
     return(
         <div>
             <h1 className='text-center text-lg font-title uppercase mb-[4rem]'>Pricing List</h1>
             <div className={PricingStyle.outerCt}>
-                
-                <div className={PricingStyle.categoryCt}>
-                    <h2 className={PricingStyle.categoryTitle}>Beard and Shave</h2>
-                    <div className={PricingStyle.priceList}>
-                        <div className={PricingStyle.listItem}>
-                            <span className='text-sm'>Beard and Line Up</span>
-                            <span>25 $</span>
-                        </div>
-                        <div className={PricingStyle.listItem}>
-                            <div className='text-sm'>Beard and Line Up</div>
-                            <p>25 $</p>
-                        </div>
-                        <div className={PricingStyle.listItem}>
-                            <span className='text-sm'>Beard and Line Up</span>
-                            <span>25 $</span>
-                        </div>
-                        
-                    </div>
-                </div>
-                
-
-            </div>
-            
+                {data?.map((category: PriceListType) => {
+                    return(
+                        <div key={category.category} className={PricingStyle.categoryCt}>
+                            <h2 className={PricingStyle.categoryTitle}>{category.category}</h2>
+                            {category.services.map(service => 
+                                <div key={service.title} className={PricingStyle.priceList}>
+                                    <div className={PricingStyle.listItem}>
+                                        <span className='text-sm'>{service.title}</span>
+                                        <span>{service.price} $</span>
+                                    </div>
+                                </div>
+                            )}
+                        </div> 
+                    );
+                })}
+            </div> 
         </div>
     )
 };
