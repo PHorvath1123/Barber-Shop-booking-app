c0619e76be388d15d150197704a09c424d03526b
PHorvath1123 <horvathpeter1996@gmail.com>
2024-09-02 18:57:48 +0200

    Fine-tuned the width of the button UI component

--- client/src/components/ui/Button.tsx
+++ client/src/components/ui/Button.tsx
@@ -14,7 +14,8 @@ export default function CustomButton(props: ButtonProps){
                 backgroundColor: '#EF6950',
                 fontFamily: 'Roboto',
                 height: '5ch',
-                width: '16ch',
+                width: 'fit-content',
+                minWidth: '16ch',
                 fontSize: 'clamp(0.6252rem, 0.9172rem + 0.2459vw, 0.8681rem)',
                 ":hover":{
                     backgroundColor: '#c9371d',
