79f0f5e7bfe6e2e5948933056941bf886554759d
PHorvath1123 <horvathpeter1996@gmail.com>
2024-09-03 17:31:37 +0200

    Imported the Calendar component to the Appointment page

--- client/src/pages/Appointment.tsx
+++ client/src/pages/Appointment.tsx
@@ -3,6 +3,7 @@ import Navbar from "../components/ui/Navbar";
 import BarberSelector from "../components/BarberSelector";
 import AppointmentStyle from "../styles/appointment/Appointment.module.css";
 import { useState } from 'react';
+import Calendar from '../components/ui/Calendar'
 
 export default function Appointment() {
     
@@ -12,6 +13,7 @@ export default function Appointment() {
         <div className={AppointmentStyle.bg}>
             <Navbar></Navbar>
             <BarberSelector selectedOption={barberId} setSelectedOption={setBarberId}></BarberSelector>
+            <Calendar></Calendar>
             <Footer></Footer>
         </div>
     );
