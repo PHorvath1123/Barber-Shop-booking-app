fa37ff311134ef1702c6ded8bbf6c6e3ce5f8f57
PHorvath1123 <horvathpeter1996@gmail.com>
2024-09-03 08:49:37 +0200

    Wrapped components in a div to apply background styling specifically to the Appointment page.

--- client/src/pages/Appointment.tsx
+++ client/src/pages/Appointment.tsx
@@ -1,14 +1,15 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/ui/Navbar";
 import BarberSelector from "../components/BarberSelector";
+import AppointmentStyle from "../styles/appointment/Appointment.module.css";
 
 export default function Appointment() {
   
     return (
-        <>
-        <Navbar></Navbar>
-        <BarberSelector></BarberSelector>
-        <Footer></Footer>
-        </>
+        <div className={AppointmentStyle.bg}>
+            <Navbar></Navbar>
+            <BarberSelector></BarberSelector>
+            <Footer></Footer>
+        </div>
     );
 }
