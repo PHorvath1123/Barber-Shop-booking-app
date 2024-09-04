eb3e01b9b84d64647fe1a9cfcf420550a544b40f
PHorvath1123 <horvathpeter1996@gmail.com>
2024-09-04 10:45:44 +0200

    Imported the Query Provider into the Appointment page and added the barberId to the Calendar component as props.

--- client/src/pages/Appointment.tsx
+++ client/src/pages/Appointment.tsx
@@ -4,17 +4,22 @@ import BarberSelector from "../components/BarberSelector";
 import AppointmentStyle from "../styles/appointment/Appointment.module.css";
 import { useState } from 'react';
 import Calendar from '../components/ui/Calendar'
+import { QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 export default function Appointment() {
     
     const [barberId, setBarberId] = useState<string>("");
+
+    const queryClient = new QueryClient()
     
     return (
         <div className={AppointmentStyle.bg}>
-            <Navbar></Navbar>
-            <BarberSelector selectedOption={barberId} setSelectedOption={setBarberId}></BarberSelector>
-            <Calendar></Calendar>
-            <Footer></Footer>
+            <Navbar/>
+            <BarberSelector selectedOption={barberId} setSelectedOption={setBarberId}/>
+            <QueryClientProvider client={queryClient}>
+                <Calendar selectedBarberId = {barberId}/>
+            </QueryClientProvider>
+            <Footer/>
         </div>
     );
 }
