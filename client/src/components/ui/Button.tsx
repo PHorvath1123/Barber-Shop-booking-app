d69d4a00a1599a929fe297e4ee31267b72cb3ffa
PHorvath1123 <horvathpeter1996@gmail.com>
2024-09-07 12:30:04 +0200

    Add a new props: variant to Button component.

--- client/src/components/ui/Button.tsx
+++ client/src/components/ui/Button.tsx
@@ -1,9 +1,15 @@
 import Button from '@mui/material/Button';
+import {colorPalette as color} from '../../utils/colorPalette'
 
 type ButtonProps = {
     text: string | number,
     type?: 'submit' | 'reset' | 'button',
-    onClick?: React.MouseEventHandler<HTMLButtonElement> 
+    onClick?: React.MouseEventHandler<HTMLButtonElement>,
+    variant?: 'text' | 'contained' | 'outlined',
+};
+
+CustomButton.defaultProps = {
+    variant: 'contained'
 };
 
 export default function CustomButton(props: ButtonProps){
@@ -11,21 +17,23 @@ export default function CustomButton(props: ButtonProps){
     return(
         <Button 
             sx={{
-                backgroundColor: '#EF6950',
+                backgroundColor: props.variant === "contained" ? color.action : "transparent",
+                color: props.variant === "text" ? color.action : null,
                 fontFamily: 'Roboto',
                 height: '5ch',
                 width: 'fit-content',
                 minWidth: '16ch',
                 fontSize: 'clamp(0.6252rem, 0.9172rem + 0.2459vw, 0.8681rem)',
                 ":hover":{
-                    backgroundColor: '#c9371d',
+                    backgroundColor: props.variant === "contained" ? color.hoverAction : 'transparent',
+                    color: props.variant !== "contained" ? color.hoverAction : null
                 },
                 '@media (min-width:768px)':{
                     height: '6ch',
                     width: '25ch'
                 },
             }} 
-            variant='contained'
+            variant={props.variant}
             type={props.type}
             onClick={props.onClick}
         >
@@ -33,3 +41,4 @@ export default function CustomButton(props: ButtonProps){
         </Button>
     );
 };
+
