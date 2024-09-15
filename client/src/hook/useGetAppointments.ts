eb2ee9243cdd988dd81ada132d3e9c0f1eec6902
PHorvath1123 <horvathpeter1996@gmail.com>
2024-09-15 18:10:15 +0200

    Create useGetAppointments custom hook to fetch the available time slots on the selected day.

---
+++ client/src/hook/useGetAppointments.ts
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+
+type appointmentType = {
+  open: string;
+  close: string;
+};
+
+
+const getAppointments = async (barberId: string, selectedDay?: string): Promise<appointmentType[]> => {
+  const request = await fetch(`/api/getAvailabilities/${barberId}/${selectedDay}`);
+  const result = await request.json();
+  return result;
+};
+
+export const useGetAppointments = (barberId: string, selectedDay?: string)  => {
+    
+  return useQuery({
+    queryKey: ["availableAppointments", barberId, selectedDay],
+    queryFn: () => getAppointments(barberId, selectedDay),
+    enabled: !!barberId && !!selectedDay,
+    staleTime: Infinity
+  });
+};
+
