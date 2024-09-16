f417528485f7e26661c9b54969bcca76e5fc38db
PHorvath1123 <horvathpeter1996@gmail.com>
2024-09-16 20:21:53 +0200

    Render the time slots in the Appointment component.

--- client/src/components/Appointment.tsx
+++ client/src/components/Appointment.tsx
@@ -1,6 +1,7 @@
 import { useGetAppointments } from '../hook/useGetAppointments';
 import {generateTimeSlots} from '../utils/generateTimeSlots';
 import { selectedDateType } from '../pages/Booking';
+import AppointmentStyle from '../styles/appointment/Appointment.module.css'
 
 type AppointmentProps = {
     selectedBarberId: string,
@@ -16,13 +17,23 @@ export default function Appointment({selectedBarberId, selectedDay}: Appointment
         error,
       } = useGetAppointments(selectedBarberId, selectedDay?.dayName);
 
-    console.log(availability);
     
     const timeSlots = availability ? generateTimeSlots(availability) : [];
 
     return(
-        <div>
-         {timeSlots}
-        </div>
+        <section>
+            <h2 className={AppointmentStyle.title}>
+                Choose an <span className="text-action font-title">appointment</span>
+            </h2>
+            <div className={AppointmentStyle.timeSlotOuterCt}>
+                <ul>
+                    {timeSlots.map(slot => {
+                        return(
+                            <button key={slot}><li>{slot}</li></button>
+                        );
+                    })}
+                </ul>
+            </div>  
+        </section> 
     );
 };
