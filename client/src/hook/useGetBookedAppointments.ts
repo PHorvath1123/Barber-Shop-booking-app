08ddfccd72d5ff4ef63d96a8e64071f98d4fe751
PHorvath1123 <horvathpeter1996@gmail.com>
2024-09-16 21:23:49 +0200

    Create the useGetBookedAppointment custom hook to fetch the bookings of the specific barber.

---
+++ client/src/hook/useGetBookedAppointments.ts
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+
+type bookingType = {
+  date: string,
+  appointment: string,
+  dayName: string
+  
+};
+
+const getBookings = async (barberId: string): Promise<bookingType[]> => {
+    const request = await fetch(`/api/getBookings/${barberId}`);
+    const result = await request.json();
+    return result;
+};
+
+export const useGetBookedAppointments = (barberId: string) => {
+    
+  return useQuery({
+    queryKey: ["bookings", barberId],
+    queryFn: () => getBookings(barberId),
+    enabled: !!barberId,
+    staleTime: Infinity
+  });
+};
