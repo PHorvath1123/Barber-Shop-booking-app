24b517eedbcc72914339e5fde4d0825f04bbebd0
PHorvath1123 <horvathpeter1996@gmail.com>
2024-10-04 18:18:17 +0200

    Create a new template in the Modal component for the successfully booking.

--- client/src/components/ui/Modal.tsx
+++ client/src/components/ui/Modal.tsx
@@ -4,29 +4,48 @@ import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import { colorPalette as color } from "../../utils/colorPalette";
 import PricingStyle from "../../styles/pricing/Pricing.module.css";
 import type { PriceListType } from "../../hook/useFetchServices";
-import AppointmentStyle from '../../styles/appointment/Appointment.module.css';
-import Button from '../ui/Button';
+import AppointmentStyle from "../../styles/appointment/Appointment.module.css";
+import Button from "../ui/Button";
 import { useServiceContext } from "../../hook/useServiceContext";
+import type { BookingResponseType } from "../../hook/usePostBooking";
+import { Link } from "react-router-dom";
 
-type ModalProps = {
-  type: "message" | "service" | "booking";
-  content?: PriceListType[];
-  open: boolean;
-  modalClose?: () => void;
-  serviceCategory?: string;
-};
+type ModalProps =
+  | {
+      type: "message";
+      content?: string;
+      open: boolean;
+      modalClose?: () => void;
+      serviceCategory?: string;
+    }
+  | {
+      type: "service";
+      content?: PriceListType[];
+      open: boolean;
+      modalClose?: () => void;
+      serviceCategory?: string;
+    }
+  | {
+      type: "booking";
+      content?: BookingResponseType;
+      open: boolean;
+      modalClose?: () => void;
+      serviceCategory?: string;
+    };
 
 const style = {
   position: "absolute" as "absolute",
-  height: "fit-content",
+  maxHeight: "90vh",
+  overflowY: "scroll",
+  overflowX: "hidden",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width:{
-    'xs': '350px',
-    'sm': '500px',
-    'md': '800px'
-  }, 
+  width: {
+    xs: "350px",
+    sm: "500px",
+    md: "800px",
+  },
   bgcolor: "#1E1A17",
   border: `2px solid ${color.light}`,
   boxShadow: 24,
@@ -45,13 +64,6 @@ const messageResponseContent = (
   </>
 );
 
-const bookingContent = (
-  <div className="flex flex-row justify-center gap-2">
-    <CheckCircleOutlineIcon sx={{ color: "#EF6950" }} fontSize="large" />
-    <h2 className="mb-3 font-bold text-lg">Success!</h2>
-  </div>
-);
-
 export default function ContactModal({
   type,
   content,
@@ -59,15 +71,17 @@ export default function ContactModal({
   modalClose,
   serviceCategory,
 }: ModalProps) {
+  const { setService } = useServiceContext();
 
-  const {setService} = useServiceContext();
-
-  const handleSelectService =(selectedService: string, price: number ) => {
-    setService({title: selectedService, price: price});
+  const handleSelectService = (selectedService: string, price: number) => {
+    setService({ title: selectedService, price: price });
     modalClose?.();
   };
 
-  const serviceContent = (serviceCategory?: string, content?: PriceListType[]) => {
+  const serviceContent = (
+    serviceCategory?: string,
+    content?: PriceListType[]
+  ) => {
     return (
       <div className="flex flex-col items-center gap-2 h-fit w-[100%]">
         {content?.map((category) => {
@@ -84,11 +98,21 @@ export default function ContactModal({
                   <div key={service.title} className={PricingStyle.priceList}>
                     <div className={AppointmentStyle.serviceListItem}>
                       <div className={AppointmentStyle.priceAndService}>
-                        <span className="text-xs sm:text-sm">{service.title}</span>
+                        <span className="text-xs sm:text-sm">
+                          {service.title}
+                        </span>
                         <span className="text-xs">{service.price} $</span>
                       </div>
                       <div className="mb-[.3rem]">
-                        <Button onClick={() => handleSelectService(service.title, service.price)} variant="outlined" size="sm">Select</Button> 
+                        <Button
+                          onClick={() =>
+                            handleSelectService(service.title, service.price)
+                          }
+                          variant="outlined"
+                          size="sm"
+                        >
+                          Select
+                        </Button>
                       </div>
                     </div>
                   </div>
@@ -101,6 +125,40 @@ export default function ContactModal({
     );
   };
 
+  const bookingContent = (content?: BookingResponseType) => {
+    return (
+      <article className="flex flex-col items-center gap-2 w-[85%] lg:w-[70%] my-0 mx-auto">
+        <div className="flex justify-center items-center gap-2">
+          <CheckCircleOutlineIcon sx={{ color: "#EF6950" }} fontSize="large" />
+          <h2 className="font-bold text-md md:text-lg">Booking Confirmed!</h2>
+        </div>
+        <div className="text-sm md:text-md flex flex-col text-left gap-7 hyphens-auto my-10">
+          <p>Thank you for your booking!</p>
+          <p>
+            We are pleased to confirm your appointment on
+            <strong className="text-action">
+              {content?.dayName}, {content?.date}
+            </strong>
+            at
+            <strong className="text-action"> {content?.appointment}</strong>.
+          </p>
+          <p>We look forward to welcoming you to our salon.</p>
+          <p className="hyphens-auto text-justify">
+            Please arrive a few minutes before your appointment to ensure
+            everything goes smoothly. Should you need to make any changes, feel
+            free to contact us in advance.
+          </p>
+        </div>
+        <p className="text-lg mb-7">See you soon!</p>
+        <div>
+          <Link to={"/"}>
+            <Button>Return to Homepage</Button>
+          </Link>
+        </div>
+      </article>
+    );
+  };
+
   return (
     <Modal open={open} onClose={modalClose} disableScrollLock={true}>
       <Box sx={style}>
@@ -109,7 +167,7 @@ export default function ContactModal({
           : type === "service"
           ? serviceContent(serviceCategory, content)
           : type === "booking"
-          ? bookingContent
+          ? bookingContent(content)
           : null}
       </Box>
     </Modal>
