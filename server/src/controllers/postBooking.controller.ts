809c21d6a4e6a77ed0646d023b9df97954d2e788
PHorvath1123 <horvathpeter1996@gmail.com>
2024-10-06 19:21:19 +0200

    Create a function to limit the number of bookings per day.

--- server/src/controllers/postBooking.controller.ts
+++ server/src/controllers/postBooking.controller.ts
@@ -2,6 +2,7 @@ import { db} from '../firebase.server.config';
 import { Request, Response } from 'express';
 import { FieldValue } from 'firebase-admin/firestore';
 import xss from 'xss';
+import { bookingCounter } from '../bookingCounter';
 
 
 type bookingDataType = {
@@ -26,18 +27,24 @@ export const addBookingToDB = async(req: Request, res: Response) => {
         comment: xss(req.body.comment)
     };
 
+    const bookingIsUnderLimit: boolean = await bookingCounter();
+
     try{
-        const newBooking = await db.collection('booking').add({
-            ...bookingData,
-            timestamp: FieldValue.serverTimestamp()
-        });
-
-        res.status(201).json({
-            dayName: bookingData.dayName,
-            date: bookingData.date,
-            appointment: bookingData.appointment
-        });
-
+        if(bookingIsUnderLimit){
+            const newBooking = await db.collection('booking').add({
+                ...bookingData,
+                timestamp: FieldValue.serverTimestamp()
+            });
+    
+            res.status(201).json({
+                dayName: bookingData.dayName,
+                date: bookingData.date,
+                appointment: bookingData.appointment
+            });
+        }else {
+            res.status(429).json({error: 'Daily booking limit reached. Try again later.'})
+        }
+        
     }catch(err){
         res.status(500).json({error: `Booking failed: ${err}`});
     }
