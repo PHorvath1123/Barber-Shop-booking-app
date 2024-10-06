fe3b56bf1b34d1185f6e77d166b94847c0082ca4
PHorvath1123 <horvathpeter1996@gmail.com>
2024-10-06 17:56:34 +0200

    Create a function to delete automatically the older bookings from the database.

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,8 +4,11 @@ import getPriceListRoute from './routes/priceList.route';
 import getWorkingTimeRoute from './routes/workingTime.route'
 import getBookingsRoute from './routes/booking.route'
 import postBookingRoute from './routes/postBooking.route'
+import {deleteOldBookings} from './deleteOldBooking'
 
 const app: Application = express();
+
+const schedule = require('node-schedule');
 
 const port = 3000;
 
@@ -16,5 +19,12 @@ app.use('/api/getPriceList', getPriceListRoute);
 app.use('/api/getAvailabilities', getWorkingTimeRoute);
 app.use('/api/getBookings', getBookingsRoute);
 app.use('/api/postBooking', postBookingRoute);
+
+
+// Schedule a job to run every day at midnight (00:00) to delete the old bookings
+// The job checks for all bookings where the appointment date is older than or equal to 5 days
+schedule.scheduleJob('* 0 0 * * *', async() => {
+    await deleteOldBookings();
+ });
 
 app.listen(port);
