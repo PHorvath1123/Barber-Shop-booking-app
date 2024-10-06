8e9f8b5efefb9a4e95242c655b2d21e66d4065d7
PHorvath1123 <horvathpeter1996@gmail.com>
2024-10-06 13:17:15 +0200

    Change the page title and favicon

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,13 @@
 import { Outlet } from "react-router-dom";
 import ServiceContextProvider from './hook/useServiceContext'
+import { useEffect } from "react";
 
 export default function App(){
+
+    useEffect(() =>{
+        document.title = 'Prestige Cuts'
+    },[])
+
     return(
        <>
             <main>
