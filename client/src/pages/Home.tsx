24b720bf86322a0e6ea2fe8a2b8fcb08dd6d1c40
PHorvath1123 <horvathpeter1996@gmail.com>
2024-10-07 21:03:46 +0200

    Add scroll to top on Home page mount

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -5,8 +5,14 @@ import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import HomeStyle from "../styles/home/Home.module.css";
 import Navbar from "../components/ui/Navbar";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  useEffect(() => {
+    window.scrollTo(0,0);
+  }, [])
+
   return (
     <>
       <div className={HomeStyle.heroContainer}>
