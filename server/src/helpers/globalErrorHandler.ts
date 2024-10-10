fb95ad6050f3aa3f0cfd7e9c4fafb8d3c6bafabc
PHorvath1123 <horvathpeter1996@gmail.com>
2024-10-10 20:57:50 +0200

    Create modular function for global error handler middleware

---
+++ server/src/helpers/globalErrorHandler.ts
@@ -0,0 +1,53 @@
+import { setTimeout } from "timers";
+import { NextFunction, Request, Response } from "express";
+
+const fetch = require("node-fetch");
+
+let errorEmailSent = false;
+let lastErrorMessage = "";
+
+export const globalErrorHandler = async (
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!errorEmailSent && lastErrorMessage !== error.message) {
+    
+    lastErrorMessage = error.message;
+
+    const object = {
+      error: error.message,
+      access_key: process.env.WEB3FORMS_ACCESS_KEY,
+      subject: "An error has occurred in Prestige Cuts website",
+    };
+
+    const json = JSON.stringify(object);
+
+    // Try to send an email with error message
+    try {
+      const request = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: json,
+      });
+
+      const response = await request.json();
+
+      if (!response.ok) {
+        throw new Error(`Failed to send error message: ${response.statusText}`);
+      } else {
+        errorEmailSent = true;
+
+        setTimeout(() => {
+          errorEmailSent = false;
+        }, 20 * 1000);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  } else next();
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,17 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+import express, { Application } from "express";
 import getBarbersRoute from "./routes/barbers.home.route";
 import getPriceListRoute from "./routes/priceList.route";
 import getWorkingTimeRoute from "./routes/workingTime.route";
 import getBookingsRoute from "./routes/booking.route";
 import postBookingRoute from "./routes/postBooking.route";
-import { deleteOldBookings } from "./deleteOldBooking";
-import { setTimeout } from "timers";
+import { deleteOldBookings } from "./helpers/deleteOldBooking";
+
 import dotenv from "dotenv";
+import { globalErrorHandler } from "./helpers/globalErrorHandler";
 
 const app: Application = express();
 
 const schedule = require("node-schedule");
-const fetch = require("node-fetch");
 dotenv.config();
 const port = 3000;
 
@@ -23,50 +23,9 @@ app.use("/api/getAvailabilities", getWorkingTimeRoute);
 app.use("/api/getBookings", getBookingsRoute);
 app.use("/api/postBooking", postBookingRoute);
 
-let errorEmailSent = false;
 
 // Global error handling middleware to catch the errors and send them via email for developing.
-app.use(
-  async (error: Error, req: Request, res: Response, next: NextFunction) => {
-    if (errorEmailSent == false) {
-      const object = {
-        error: error.message,
-        access_key: process.env.WEB3FORMS_ACCESS_KEY,
-        subject: "An error has occurred in Prestige Cuts website",
-      };
-
-      const json = JSON.stringify(object);
-
-      // Try to send an email with error message
-      try {
-        const request = await fetch("https://api.web3forms.com/submit", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-          },
-          body: json,
-        });
-
-        const response = await request.json();
-
-        if (!response.ok) {
-          throw new Error(
-            `Failed to send error message: ${response.statusText}`
-          );
-        } else {
-          errorEmailSent = true;
-
-          setTimeout(() => {
-            errorEmailSent = false;
-          }, 60 * 1000);
-        }
-      } catch (err) {
-        res.status(500).send(err);
-      }
-    } else next();
-  }
-);
+app.use(globalErrorHandler);
 
 // Schedule a job to run every day at midnight (00:00) to delete the old bookings
 // The job checks for all bookings where the appointment date is older than or equal to 5 days
