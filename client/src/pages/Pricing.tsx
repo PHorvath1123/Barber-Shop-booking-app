42470b575d2e67a78eaa947796a4bb68b0f5808a
PHorvath1123 <horvathpeter1996@gmail.com>
2024-10-19 17:52:19 +0200

    Add a new background to the Price list.

--- client/src/pages/Pricing.tsx
+++ client/src/pages/Pricing.tsx
@@ -2,12 +2,14 @@ import Navbar from "../components/ui/Navbar";
 import Footer from "../components/Footer";
 import PriceList from "../components/PriceList";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import PricingStyle from "../styles/pricing/Pricing.module.css";
 
 const queryClient = new QueryClient();
 
 export default function Pricing() {
+
   return (
-    <>
+    <div className={PricingStyle.bg}>
       <Navbar></Navbar>
       <main>
         <QueryClientProvider client={queryClient}>
@@ -15,6 +17,6 @@ export default function Pricing() {
         </QueryClientProvider>
       </main>
       <Footer></Footer>
-    </>
+    </div>
   );
 }
