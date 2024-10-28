cae018f0893e0a3461d40061f98a6171954a6b54
PHorvath1123 <horvathpeter1996@gmail.com>
2024-10-28 18:37:04 +0100

    Set document title in index.html

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,9 @@
 import { Outlet } from "react-router-dom";
 import ServiceContextProvider from "./hook/useServiceContext";
-import { useEffect } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function App() {
     
-  useEffect(() => {
-    document.title = "Prestige Cuts";
-  }, []);
-
   const queryClient = new QueryClient();
 
   return (
