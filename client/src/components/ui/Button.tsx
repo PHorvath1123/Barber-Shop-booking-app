477c62b8ff956df07793e996ad1b489da1e2b814
PHorvath1123 <horvathpeter1996@gmail.com>
2025-01-20 20:08:59 +0100

    Add "lg" size props value to the Button component

--- client/src/components/ui/Button.tsx
+++ client/src/components/ui/Button.tsx
@@ -6,7 +6,7 @@ type ButtonProps = {
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   variant?: "contained" | "outlined";
   children: React.ReactNode;
-  size?: "sm";
+  size?: "sm" | "lg";
   disabled?: boolean;
 };
 
@@ -41,7 +41,7 @@ export default function CustomButton({
     fontFamily: "Roboto",
     height: "5ch",
     width: "fit-content",
-    minWidth: "16ch",
+    minWidth: size === "lg" ? "35ch" : "16ch",
     fontSize: "clamp(0.6252rem, 0.9172rem + 0.2459vw, 0.8681rem)",
     ":hover": {
       backgroundColor: props.type === "reset" ? "#292929" : color.hoverAction,
