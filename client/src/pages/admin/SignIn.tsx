8095bfdbfe786df47dcd9e118826e3393bd3a7bd
PHorvath1123 <horvathpeter1996@gmail.com>
2025-01-22 18:07:23 +0100

    Add validation to the SignIn component

--- client/src/pages/admin/SignIn.tsx
+++ client/src/pages/admin/SignIn.tsx
@@ -8,15 +8,58 @@ import Button from "../../components/ui/Button";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import IconButton from "@mui/material/IconButton";
+import Mustache from "/mustache.png";
+import { z } from "zod";
+import AppointmentStyle from "../../styles/appointment/Appointment.module.css";
+
+const emailSchema = z
+  .string()
+  .email({ message: "Must be a valid email address!" });
+
+const passwordSchema = z
+  .string()
+  .regex(/^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z0-9]+$/gm, {
+    message: "Must contain at least one uppercase letter and one number.",
+  });
+
+type ValidationError = {
+  _errors: string[];
+  email?: { _errors: string[] };
+  password?: { _errors: string[] };
+};
 
 export default function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [validationError, setValidationError] = useState<ValidationError>();
 
   const toggleShowPassword = () => {
     setShowPassword(!showPassword);
-  }
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const emailIsValid = emailSchema.safeParse(email);
+    const passwordIsValid = passwordSchema.safeParse(password);
+
+    setValidationError({
+      _errors: [],
+      email: emailIsValid.success
+        ? undefined
+        : { _errors: emailIsValid.error.format()._errors },
+      password: passwordIsValid.success
+        ? undefined
+        : { _errors: passwordIsValid.error.format()._errors },
+    });
+
+    if (emailIsValid.success && passwordIsValid.success) {
+      setEmail("");
+      setPassword("");
+      setValidationError({ _errors: [] });
+    }
+  };
 
   return (
     <>
@@ -24,12 +67,16 @@ export default function SignIn() {
         <div className="relative">
           <div className={SigninStyle.loginColorCt}></div>
           <Navbar />
-          <section>
+          <section className={SigninStyle.loginOuterCt}>
             <div className={SigninStyle.loginBg}>
               <AccountCircleIcon sx={{ fontSize: "50px" }}></AccountCircleIcon>
+              <img src={Mustache} alt="Mustache" className="w-[80px]" />
               <h1 className={SigninStyle.login}>Login</h1>
             </div>
-            <form className={SigninStyle.form}>
+            <form
+              className={SigninStyle.form}
+              onSubmit={(e) => handleSubmit(e)}
+            >
               <TextInput
                 name="Email"
                 label="E-mail"
@@ -38,6 +85,11 @@ export default function SignIn() {
                 }}
                 value={email}
               />
+              {validationError?.email && (
+                <p className={AppointmentStyle.error}>
+                  {validationError?.email._errors[0]}
+                </p>
+              )}
               <span className="w-[100%] relative mb-5">
                 <TextInput
                   name="Password"
@@ -58,21 +110,21 @@ export default function SignIn() {
                   }}
                   onClick={toggleShowPassword}
                 >
-                  {showPassword ? (
-                    <VisibilityIcon/>
-                  ) : (
-                    <VisibilityOffIcon/>
-                  )}
+                  {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
                 </IconButton>
               </span>
-
+              {validationError?.password && (
+                <p className={AppointmentStyle.error}>
+                  {validationError?.password._errors[0]}
+                </p>
+              )}
               <Button type="submit" variant="contained" size="lg">
                 Login
               </Button>
             </form>
           </section>
         </div>
-        <Footer></Footer>
+        <Footer />
       </main>
     </>
   );
