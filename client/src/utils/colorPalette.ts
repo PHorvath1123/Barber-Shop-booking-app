d4bb67707a5fccd82f8abb9f060cd639fa969984
PHorvath1123 <horvathpeter1996@gmail.com>
2025-02-04 19:45:01 +0100

    Add more colors to the colorPalette.

--- client/src/utils/colorPalette.ts
+++ client/src/utils/colorPalette.ts
@@ -3,5 +3,8 @@ export const colorPalette = {
     dark: '#1E1A17',
     light: '#D9D9D9',
     hoverAction: '#c9371d',
-    green: '#64A332'
+    green: '#64A332',
+    adminMainDark: "#242220FF",
+    adminCardDark: "#1E1A17AB",
+    adminHeaderDark: "#1E1A17"
 };
